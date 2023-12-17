d915d0a4e840360b78aa499291fd9645a41849e5
Priyanshu Arya <65180123+YASTREAMER@users.noreply.github.com>
2023-12-18 01:13:02 +0530

    Added the main function and README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chess Repo
+
+This is my chess engine repo. I have created it to test out a chess engine I am creting.
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    /* code */
+
+    cout<<"Hello world";
+
+    return 0;
+}
